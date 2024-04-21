b40303a3aae1cf91e814f202f9d3ef53dc26e06e
James Tennant <jorahty@gmail.com>
2024-04-20 20:22:05 -0700

    define --link css var

--- app/contact.tsx
+++ app/contact.tsx
@@ -12,8 +12,8 @@ export default function Contact() {
   ) : (
     <div
       onClick={() => setClicked(true)}
-      className="text-center text-blue-500 font-semibold hover:cursor-pointer">
-      Contact me?
+      className="text-center text-[color:var(--link)] font-semibold hover:cursor-pointer">
+      Contact me
     </div>
   );
 }
