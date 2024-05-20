5a4c130aa5f501ccb4770f7c0ac20b39f96cbe87
James Tennant <jorahty@gmail.com>
2024-05-19 22:15:22 -0700

    add project descriptions

--- app/projects/hyneria/page.tsx
+++ app/projects/hyneria/page.tsx
@@ -1,11 +1,23 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { projects } from '@/data/projects';
+
 import { Button } from '@/components/ui/button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 export default function Home() {
+  const pathname = usePathname();
+  const project = projects.find(project => project.title.toLowerCase() === pathname.split('/').pop())!
   return (
     <main className="p-4 flex flex-col items-start gap-4 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold">Hyneria</h1>
+
+      <div className='mb-4'>
+        <p className="text-sm">{project.description}</p>
+        <p className="text-sm text-muted-foreground">{project.details}</p>
+      </div>
 
       <TooltipProvider>
         <Tooltip delayDuration={0}>
--- app/projects/probability-puzzle/page.tsx
+++ app/projects/probability-puzzle/page.tsx
@@ -1,10 +1,23 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { projects } from '@/data/projects';
+
 import { Button } from '@/components/ui/button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Home() {
+  const pathname = usePathname();
+  const project = projects.find(project => project.title.toLowerCase() === pathname.split('/').pop())!
+  
   return (
     <main className="p-4 flex flex-col items-start gap-4 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold">Probability-Puzzle</h1>
+
+      <div className='mb-4'>
+        <p className="text-sm">{project.description}</p>
+        <p className="text-sm text-muted-foreground">{project.details}</p>
+      </div>
 
       <a href="https://wheel.jorahty.com">
         <Button variant="outline">View Deployed Application</Button>
